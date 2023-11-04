6d530bdd9639f225377c54f39326d116817e05be
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-11-04 16:18:33 +0600

    Add: Blob Service for uploading File

---
+++ Accessories.DataAccess/Repository/Services/BlobService.cs
@@ -0,0 +1,54 @@
+﻿using Accessories.DataAccess.Repository.IRepository.Services;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accessories.DataAccess.Repository.Services;
+
+public class BlobService : IBlobService
+{
+    private readonly BlobServiceClient _blobServiceClient;
+
+    public BlobService(BlobServiceClient blobServiceClient)
+    {
+        _blobServiceClient = blobServiceClient;
+    }
+
+    public async Task<string> GetBlob(string blobName, string containerName)
+    {
+        BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+        BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+        return await Task.FromResult(blobClient.Uri.AbsoluteUri);
+    }
+
+    public async Task<string> UploadBlob(string blobName, string containerName, IFormFile file)
+    {
+        BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+        BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+        var httpHeaders = new BlobHttpHeaders
+        {
+            ContentType = file.ContentType
+        };
+
+        var response = await blobClient.UploadAsync(file.OpenReadStream(), httpHeaders);
+        if (response != null)
+            return await GetBlob(blobName, containerName);
+
+        return "";
+    }
+
+    public async Task<bool> DeleteBlob(string blobName, string containerName)
+    {
+        BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+        BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+        return await blobClient.DeleteIfExistsAsync();
+    }
+}
