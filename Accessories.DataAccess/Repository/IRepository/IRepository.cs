02bc1bd24a08c7081f3f29d95da413dff168c6de
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-11-04 14:47:20 +0600

    Add: Repository for data abstruction

---
+++ Accessories.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accessories.DataAccess.Repository.IRepository;
+
+public interface IRepository<T> where T : class
+{
+    Task<T> GetFirstOrDefaultAsync(Expression<Func<T, bool>> filter);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    void RemoveAsync(T entity);
+    void RemoveRangeAsync(IEnumerable<T> entity);
+    Task<T> GetDetailsAsync(int id);
+    Task SaveAsync();
+}
---
+++ Accessories.DataAccess/Repository/Repository.cs
@@ -0,0 +1,68 @@
+﻿using Accessories.DataAccess.Data;
+using Accessories.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accessories.DataAccess.Repository;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly ApplicationDbContext _db;
+    internal DbSet<T> dbSet;
+
+    public Repository(ApplicationDbContext db)
+    {
+        _db = db;
+        this.dbSet = _db.Set<T>();
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await dbSet.AddAsync(entity);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        var data = await dbSet.ToListAsync();
+        return data;
+    }
+
+    public async Task<T> GetDetailsAsync(int id)
+    {
+        var data = await dbSet.FindAsync(id);
+        return data;
+    }
+
+    public async Task<T> GetFirstOrDefaultAsync(Expression<Func<T, bool>> filter)
+    {
+        IQueryable<T> query = dbSet;
+        query = query.Where(filter);
+
+        return await query.FirstOrDefaultAsync();
+    }
+
+    public void RemoveAsync(T entity)
+    {
+        dbSet.Remove(entity);
+    }
+
+    public void RemoveRangeAsync(IEnumerable<T> entity)
+    {
+        dbSet.RemoveRange(entity);
+    }
+
+    public Task SaveAsync()
+    {
+        return _db.SaveChangesAsync();
+    }
+
+    public Task UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
