3d9f10ec42d0bc222d0c175908fd0f47b33f67f4
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-11-04 14:34:39 +0600

    Add: ApiResponse as a Utils service

---
+++ Accessories.Models/Utils/ApiResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accessories.Models.Utils;
+
+public class ApiResponse
+{
+    public ApiResponse()
+    {
+        ErrorMessages = new List<string>();
+    }
+    public List<string> ErrorMessages { get; set; }
+    public bool IsSuccess { get; set; } = true;
+    public object Result { get; set; }
+    public HttpStatusCode StatusCode { get; set; }
+}
